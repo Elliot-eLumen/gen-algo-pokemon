d9455834437fe2219349dc1ec4b0dc4574799b0c
Matt Gordon <matt_gordon@elumen.info>
2022-12-19 15:01:51 -0600

    route setup and setup page start

--- pokemon-team-optimization/src/App.js
+++ pokemon-team-optimization/src/App.js
@@ -1,7 +1,10 @@
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Card } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Setup from "./Routes/Setup";
 
 function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -19,7 +22,14 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>Pokemon Team Optimization</div>
+      <Router>
+        <Card sx={{ padding: "15px", textAlign: "center" }}>
+          <header>Pokemon Team Optimization</header>
+        </Card>
+        <Routes>
+          <Route path="/" exact element={<Setup />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ pokemon-team-optimization/src/Routes/Setup/index.js
@@ -0,0 +1,87 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+const Setup = () => {
+  const [poolSize, setPoolSize] = useState("");
+  const [rateArray, setRateArray] = useState([]);
+  const [rate, setRate] = useState("");
+  const [fitnessFunction, setFitnessFunction] = useState("");
+  const handleSizeChange = (event) => {
+    setPoolSize(event.target.value);
+    setRateArray(Array.from({ length: event.target.value }, (v, i) => i));
+  };
+  const handleRateChange = (event) => {
+    setRate(event.target.value);
+  };
+  const handleFitnessChange = (event) => {
+    setFitnessFunction(event.target.value);
+  };
+  return (
+    <div>
+      <FormControl fullWidth>
+        <InputLabel id="team-pool-size-label">Team Pool Size</InputLabel>
+        <Select
+          labelId="team-pool-size-label"
+          id="team-pool-size"
+          value={poolSize}
+          label="Team Pool Size"
+          onChange={handleSizeChange}
+        >
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={20}>20</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="mutation-rate-label">Mutation Rate</InputLabel>
+        <Select
+          labelId="mutation-rate-label"
+          id="mutation-rate"
+          value={rate}
+          label="Mutation Rate"
+          onChange={handleRateChange}
+        >
+          {rateArray.length > 0 &&
+            rateArray.map((option, index) => {
+              return (
+                <MenuItem key={index} value={option + 1}>
+                  {option + 1}
+                </MenuItem>
+              );
+            })}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="fitness-function-label">Fitness Function</InputLabel>
+        <Select
+          labelId="fitness-function-label"
+          id="fitness-function"
+          value={fitnessFunction}
+          label="Fitness Function"
+          onChange={handleFitnessChange}
+        >
+          <MenuItem value={"stats"}>Stats</MenuItem>
+          <MenuItem value={"general"}>General Coverage</MenuItem>
+          <MenuItem value={"specific"}>Specific Coverage</MenuItem>
+          <MenuItem value={"combo"}>Combo (Stats and Specific)</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        id="outlined-number"
+        label="Number"
+        type="number"
+        InputProps={{ inputProps: { min: 0, max: 1000 } }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Setup;
