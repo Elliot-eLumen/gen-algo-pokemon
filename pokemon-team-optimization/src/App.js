a791e1ad809b7c76e455e30acbcb65a78af6f413
Matt Gordon <matt_gordon@elumen.info>
2022-12-19 15:48:20 -0600

    layout updates

--- pokemon-team-optimization/src/App.js
+++ pokemon-team-optimization/src/App.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Card } from "@mui/material";
+import { Card, Container, Typography } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Setup from "./Routes/Setup";
+import Result from "./Routes/Result";
 
 function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -22,14 +23,31 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Card sx={{ padding: "15px", textAlign: "center" }}>
-          <header>Pokemon Team Optimization</header>
-        </Card>
-        <Routes>
-          <Route path="/" exact element={<Setup />} />
-        </Routes>
-      </Router>
+      <Card sx={{ padding: "15px 0", marginBottom: 2 }}>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            pl: 4,
+            fontWeight: 700,
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Pokemon Team Optimization
+        </Typography>
+      </Card>
+      <Container maxWidth="100%">
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<Setup />} />
+            <Route path="/result" exact element={<Result />} />
+          </Routes>
+        </Router>
+      </Container>
     </ThemeProvider>
   );
 }
