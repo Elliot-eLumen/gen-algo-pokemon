0074eda944f04d3008c9aa107730115a829c90a3
Matt Gordon <matt_gordon@elumen.info>
2022-12-20 17:18:28 -0600

    corrected order

--- pokemon-team-optimization/src/Routes/Pokedex/index.js
+++ pokemon-team-optimization/src/Routes/Pokedex/index.js
@@ -11,7 +11,12 @@ const Pokedex = () => {
   const images = importAll(
     require.context("../../../images", false, /\.(png|jpe?g|svg)$/)
   );
-  const imageArray = Object.entries(images);
+  const imageValues = Object.values(images);
+  const imageKeys = Object.keys(images);
+  let finalImageArray = [];
+  imageKeys.forEach((image, key) => {
+    finalImageArray[image.replace(".png", "")] = imageValues[key];
+  });
 
   return (
     <div>
@@ -19,20 +24,19 @@ const Pokedex = () => {
         Pokedex
       </Typography>
       <Grid container spacing={2}>
-        {imageArray &&
-          imageArray.map((image, index) => {
-            return (
-              <Grid item key={index}>
-                <Card key={index} sx={{ textAlign: "center" }}>
-                  <img
-                    src={images[image[0]]}
-                    alt={`pokemon-${index}`}
-                    style={{ height: "75px" }}
-                  />
-                </Card>
-              </Grid>
-            );
-          })}
+        {finalImageArray.map((image, index) => {
+          return (
+            <Grid item key={index}>
+              <Card key={index} sx={{ textAlign: "center" }}>
+                <img
+                  src={image}
+                  alt={`pokemon-${index}`}
+                  style={{ height: "75px" }}
+                />
+              </Card>
+            </Grid>
+          );
+        })}
       </Grid>
     </div>
   );
