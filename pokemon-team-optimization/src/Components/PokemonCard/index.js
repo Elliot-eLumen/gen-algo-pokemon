768aeb52b385496832e350627afa6a84d4e55cd8
Matt Gordon <matt_gordon@elumen.info>
2022-12-22 11:03:55 -0600

    making the detail card work

--- pokemon-team-optimization/src/Components/PokemonCard/index.js
+++ pokemon-team-optimization/src/Components/PokemonCard/index.js
@@ -8,71 +8,20 @@ import {
 } from "@mui/material";
 import { useContext } from "react";
 import { Images } from "../../Utils/Context";
+import handleColor from "../../Utils/Color";
 
-const handleColor = (data) => {
-  switch (data[0]) {
-    case "water":
-      return "#6390F0";
-
-    case "ice":
-      return "#96D9D6";
-
-    case "grass":
-      return "#7ac74c";
-
-    case "bug":
-      return "#a6b91a";
-
-    case "ground":
-      return "#e2bf65";
-
-    case "rock":
-      return "#b6a136";
-
-    case "electric":
-      return "#f7d02c";
-
-    case "ghost":
-      return "#735797";
-
-    case "fighting":
-      return "#c22e28";
-
-    case "fire":
-      return "#ee8130";
-
-    case "poison":
-      return "#a33ea1";
-
-    case "flying":
-      return "#a98ff3";
-
-    default:
-      return "#a8a77a";
-  }
-};
-
-const DetailsCard = () => {
-  const imgArray = useContext(Images);
+export const DetailsCard = ({ data, image }) => {
+  // const imgArray = useContext(Images);
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140, backgroundColor: "lightgreen" }}
-        image={imgArray[1]}
+        image={image}
         title="bulbasaur"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Bulbasaur
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Bulbasaur is a small, quadrupedal Pokémon that has blue-green skin
-          with darker patches. It has red eyes with white pupils, pointed,
-          ear-like structures on top of its head, and a short, blunt snout with
-          a wide mouth. A pair of small, pointed teeth are visible in the upper
-          jaw when its mouth is open. Each of its thick legs ends with three
-          sharp claws. On Bulbasaur's back is a green plant bulb, which is grown
-          from a seed planted there at birth.
         </Typography>
       </CardContent>
     </Card>
@@ -86,6 +35,8 @@ export const ResultCard = ({ data }) => {
     <Card
       sx={{
         maxWidth: 345,
+        border: `1px solid`,
+        borderColor: handleColor(data.typing),
       }}
     >
       <CardMedia
--- pokemon-team-optimization/src/Routes/Pokedex/index.js
+++ pokemon-team-optimization/src/Routes/Pokedex/index.js
@@ -1,24 +1,41 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Card, Grid, Popover, Typography } from "@mui/material";
 import { useContext, useState } from "react";
-import { Images } from "../../Utils/Context";
+import { DetailsCard } from "../../Components/PokemonCard";
+import { Images, PokemonData } from "../../Utils/Context";
+import handleColor from "../../Utils/Color";
 
 const Pokedex = () => {
   const imgArray = useContext(Images);
-  const [mon, setMon] = useState({});
+  const pokemonData = useContext(PokemonData);
+  // const [mon, setMon] = useState({});
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [detailData, setDetailData] = useState({});
 
-  const handleClick = (event) => {
-    fetch(
-      "http://127.0.0.1:5000/pokemon?" + new URLSearchParams({ id: event }),
-      {
-        method: "get",
-      }
-    ).then((response) => {
-      response.json().then((mon) => {
-        console.log(mon);
-      });
-    });
+  const handleOpen = (data) => (event) => {
+    setDetailData(data);
+    setAnchorEl(event.currentTarget);
   };
-  console.log(mon);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "detail-popover" : undefined;
+
+  // const handleClick = (event) => {
+  //   fetch(
+  //     "http://127.0.0.1:5000/pokemon?" + new URLSearchParams({ id: event }),
+  //     {
+  //       method: "get",
+  //     }
+  //   ).then((response) => {
+  //     response.json().then((mon) => {
+  //       console.log(mon);
+  //     });
+  //   });
+  // };
+  // console.log(mon);
 
   return (
     <>
@@ -32,9 +49,16 @@ const Pokedex = () => {
               return (
                 <Grid item key={index}>
                   <Card
+                    aria-describedby={id}
                     key={index}
-                    sx={{ textAlign: "center" }}
-                    onClick={() => handleClick(index)}
+                    sx={{
+                      textAlign: "center",
+                      border: "none",
+                      // border: "1px solid #ccc",
+                      cursor: "pointer",
+                    }}
+                    component="button"
+                    onClick={handleOpen(pokemonData[index])}
                   >
                     <img
                       src={image}
@@ -42,6 +66,18 @@ const Pokedex = () => {
                       style={{ height: "75px" }}
                     />
                   </Card>
+                  <Popover
+                    id={id}
+                    open={open}
+                    onClose={handleClose}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                  >
+                    <DetailsCard data={detailData} image={image} />
+                  </Popover>
                 </Grid>
               );
             })}
